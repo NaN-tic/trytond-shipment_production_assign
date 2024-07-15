d6cbf2c26e4ca0fb9cf070b3a317d62816243c8e
Deniz Ceylan <deniz.ceylan@nan-tic.com>
2024-07-15 11:15:31 +0200

    Added start of test to allow pull request task #165550

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,10 +2,22 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.modules.company.tests import (
+    CompanyTestMixin, create_company, set_company)
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.transaction import Transaction
+from trytond.pool import Pool
 
 
 class ShipmentProductionAssignTestCase(ModuleTestCase):
     'Test Shipment Production Assign module'
     module = 'shipment_production_assign'
+
+    @with_transaction()
+    def test_party_identifiers(self):
+        'Party Identifiers'
+        pool = Pool()
+        company = create_company()
+
 
 del ModuleTestCase
