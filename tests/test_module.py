4270e585a478e6d752ff892d3dbe96b5acc72cfe
Albert Cervera i Areny <albert@nan-tic.com>
2024-07-16 07:34:59 +0200

    Pyflakes fixes.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,23 +1,13 @@
 # This file is part shipment_production_assign module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.modules.company.tests import (
-    CompanyTestMixin, create_company, set_company)
-from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.transaction import Transaction
-from trytond.pool import Pool
 
 
-class ShipmentProductionAssignTestCase(ModuleTestCase):
+class ShipmentProductionAssignTestCase(ModuleTestCase, CompanyTestMixin):
     'Test Shipment Production Assign module'
     module = 'shipment_production_assign'
-
-    @with_transaction()
-    def test_party_identifiers(self):
-        'Party Identifiers'
-        pool = Pool()
-        company = create_company()
 
 
 del ModuleTestCase
